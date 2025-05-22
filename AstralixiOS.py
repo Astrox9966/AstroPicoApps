3b4d5c90d6a599aee4d18205d5c409f524ec097f
Astrox9966 <bhavnapiyushp@gmail.com>
2025-05-22 15:48:51 +0800

    Delete File Contents

--- AstralixiOS.py
+++ AstralixiOS.py
@@ -1,6 +1,1 @@
-black = rgb("0, 0, 0")
-white = rgb("255, 255, 255")
-lightgrey = rgb("211, 211, 211")  ' Light grey
-darkgrey = rgb("169, 169, 169")   ' Dark grey
-grey = rgb("128, 128, 128")       ' Standard grey
 
